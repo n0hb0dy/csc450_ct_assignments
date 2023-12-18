9e3cbba9ed86e14c16222215df3dffaa8b4b7bcd
n0hb0dy <36122937+n0hb0dy@users.noreply.github.com>
2023-12-17 22:48:21 -0500

    module 1 completed

---
+++ Module One/CSC450_CT1_mod1-2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+//Standard namespace declaration
+using namespace std;
+
+//Main Function
+int main()
+{
+
+     double myMoney = 1000.50;  //this should be printed out
+
+
+     //Standard Ouput Statement
+     cout<<"Please be sure to correct all syntax errors in this program"<<endl;
+
+     cout<<"I have corrected all errors for this program." <<endl;
+
+     cout<<" The total amount of money available is = " << myMoney<<endl;
+
+     //Main Function return Statement
+     return 0;
+}
